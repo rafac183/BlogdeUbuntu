f60908bdefe1923f2776724329a33f468bd34ea8
Rafael Cordero <rafaelantoniocordero2002@gmail.com>
2022-02-24 18:22:35 -0300

    Ultimo commit de Programacion Básica

--- pb/docu/codigo.js
+++ pb/docu/codigo.js
@@ -1,4 +1,6 @@
 var ruta = window.location;
 console.log(ruta)
 //esto imprime en html
-document.write("Estás en " + ruta);
+document.write("Estás en " + ruta + "<br />");
+document.write("ja" + "ja" + "\n" + "ja" + "ja");
+//\n= funciona como barra espaciadora
